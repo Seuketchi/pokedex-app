942c3974c00566d34b8a06742eeff2fedf2da83d
Seuketchi <jadman.tristan10@gmail.com>
2025-12-25 03:23:25 +0800

    chore: update analysis options to exclude generated files and adjust formatting check in CI

--- lib/features/pokemon_detail/domain/entities/evolution_node.dart
+++ lib/features/pokemon_detail/domain/entities/evolution_node.dart
@@ -7,9 +7,9 @@ class EvolutionNode with _$EvolutionNode {
   const factory EvolutionNode({
     required String speciesName,
     required int speciesId,
+    required List<EvolutionNode> evolvesTo,
     String? trigger,
     int? minLevel,
     String? item,
-    required List<EvolutionNode> evolvesTo,
   }) = _EvolutionNode;
 }
